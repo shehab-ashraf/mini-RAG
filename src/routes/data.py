5a9b6249a934c0a90b04492b5c95830a6fe28fed
shehab-ashraf <ashrafshehab377@gmail.com>
2026-01-24 07:43:47 +0200

    feat: define API routes for base and data endpoints

---
+++ src/routes/data.py
@@ -0,0 +1,58 @@
+import os
+import logging
+import aiofiles
+
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
+from fastapi.responses import JSONResponse
+
+from helpers.config import get_settings, Settings
+from controllers import DataController, ProjectController
+from models import ResponseSignal
+
+
+logger = logging.getLogger("uvicorn.error")
+
+data_router = APIRouter(
+    prefix="/api/v1/data",
+    tags=["api_v1", "data"]
+)
+
+@data_router.post("/upload/{project_id}")
+async def upload_data(project_id: str, file: UploadFile, app_settings: Settings = Depends(get_settings)):
+
+    # validate upload data
+    is_valid, result_signal = DataController().validate_upload_data(file=file)
+
+    if not is_valid:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"signal": result_signal})
+
+    project_dir_path = ProjectController().get_project_path(project_id=project_id)
+    print(project_dir_path)
+    file_path = os.path.join(project_dir_path, file.filename)
+    print(file_path)
+    file_path, file_id = DataController().generate_unique_filepath(
+        original_file_name=file.filename,
+        project_id=project_id
+    )
+
+    try:
+        async with aiofiles.open(file_path, "wb") as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+
+        logger.error(f"Error while uploading file: {e}")
+
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_FAILED.value
+            }
+        )
+    
+    return JSONResponse(
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+                "file_id": file_id
+            }
+        )
