6bba6e8ad694f4de6e4fcdbb5234118e05abaa0a
shehab-ashraf <ashrafshehab377@gmail.com>
2026-01-24 10:06:50 +0200

    feat: add ProcessController for text extraction and splitting

---
+++ src/controllers/ProcessController.py
@@ -0,0 +1,60 @@
+import os
+from .BaseController import BaseController
+from .ProjectController import ProjectController
+from langchain_community.document_loaders import TextLoader, PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from models.enums.ProcessingEnums import ProcessingEnums
+
+
+class ProcessController(BaseController):
+
+    def __init__(self, project_id: str):
+        super().__init__()
+
+        self.project_id = project_id
+        self.project_path = ProjectController().get_project_path(project_id=project_id)
+
+    def get_file_extension(self, file_id: str):
+        return os.path.splitext(file_id)[-1]
+
+    def get_file_loader(self, file_id: str):
+
+        file_extension = self.get_file_extension(file_id=file_id)
+        file_path = os.path.join(self.project_path, file_id)
+
+        if file_extension == ProcessingEnums.TXT.value:
+            return TextLoader(file_path=file_path, encoding="utf-8")
+        elif file_extension == ProcessingEnums.PDF.value:
+            return PyMuPDFLoader(file_path=file_path)
+
+        return None
+
+    def get_file_content(self, file_id: str):
+
+        loader = self.get_file_loader(file_id=file_id)
+        return loader.load()
+    
+    def process_file_content(self, file_content: list, file_id: str, chunk_size: int=100, overlap_size: int=20):
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size, 
+            chunk_overlap=overlap_size, 
+            length_function=len
+        )
+
+        file_content_texts = [
+            rec.page_content
+            for rec in file_content
+        ]
+
+        file_content_metadata = [
+            rec.metadata
+            for rec in file_content
+        ]
+
+        chunks = text_splitter.create_documents(
+           file_content_texts,
+            metadatas=file_content_metadata
+        )
+
+        return chunks
+    
